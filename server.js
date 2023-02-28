9fb5b31f2bed99576901cdc4a6cc32f225717861
jeongyongman <i1683u@gmail.com>
2023-02-28 16:21:17 +0900

    pet beauty 경로추가

--- server.js
+++ server.js
@@ -5,3 +5,27 @@ const port = 8080;
 app.listen(port, () => {
   console.log(`========== express 서버시작 ==========\nlistening on port ${port}`)
 });
+
+app.get('/pet', function (req, res) {
+  res.send('펫용품 쇼핑할 수 있는 페이지입니다.')
+});
+
+app.get('/beauty', function (req, res) {
+  res.send('뷰티용품 쇼핑할 수 있는 페이지입니다.')
+});
+// app.get('/sound/:name', function (req, res) {
+//   // const p = req.params
+//   // console.log(p.name)
+//   const { name } = req.params
+//   if (name == "dog") {
+//     res.json({'sound' : '멍멍'})
+//   } else if (name == "cat") {
+//     res.json({'sound' : '야옹'})
+//   } else if (name == "pig") {
+//     res.json({'sound' : '꿀꿀'})
+//   } else {
+//     res.json({'sound' : '지정되지 않았음'})
+
+//   }
+  
+// })
